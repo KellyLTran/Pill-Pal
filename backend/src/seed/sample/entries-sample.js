cec60ab16f15157785241646db2585e48da4705d
mattcattb <mboughton17@gmail.com>
2025-01-25 16:08:55 -0500

    added seeded data to database

---
+++ backend/src/seed/sample/entries-sample.js
@@ -0,0 +1,10 @@
+export const entries = [
+  {
+    usedAt: new Date(),
+    medication: null, // Will be populated after medications are created
+  },
+  {
+    usedAt: new Date(),
+    medication: null, // Will be populated after medications are created
+  },
+];
---
+++ backend/src/seed/sample/medications-sample.js
@@ -0,0 +1,26 @@
+export const medications = [
+  {
+    name: 'Medication A',
+    dosage: 500,
+    release: 'IR',
+    sleep_m: 6,
+    concentration_map: new Map([
+      ['hour1', 10],
+      ['hour2', 20],
+      ['hour3', 30],
+    ]),
+  },
+  {
+    name: 'Medication B',
+    dosage: 250,
+    release: 'EX',
+    sleep_m: 8,
+    concentration_map: new Map([
+      ['hour1', 15],
+      ['hour2', 25],
+      ['hour3', 35],
+    ]),
+  },
+];
+
+
